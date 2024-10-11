2eb3655a6372c637223f4d7c0b7a6919581ee662
michAele13 <amanda.marrero13@gmail.com>
2024-10-11 15:25:41 -0500

    Added javascript to update times in cities shown

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,26 @@
-alert("hi, hello, how are you?");
+function updateTime() {
+let sanMarcosElement = document.querySelector("#san-marcos");
+let sanMarcosDateElement = sanMarcosElement.querySelector(".date");
+let sanMarcosTimeElement = sanMarcosElement.querySelector(".time");
+
+let sanMarcosTime = moment().tz("America/Ojinaga");
+
+sanMarcosDateElement.innerHTML = sanMarcosTime.format("MMMM Do YYYY");
+sanMarcosTimeElement.innerHTML = sanMarcosTime.format("h:mm:ss [<small>]A[</small>]");
+
+
+    let shanghaiElement = document.querySelector("#shanghai");
+    let shanghaiDateElement = shanghaiElement.querySelector(".date");
+    let shanghaiTimeElement = shanghaiElement.querySelector(".time");
+    
+    let shanghaiTime = moment().tz("Asia/Shanghai");
+    
+    shanghaiDateElement.innerHTML = shanghaiTime.format("MMMM Do YYYY");
+    shanghaiTimeElement.innerHTML = shanghaiTime.format("h:mm:ss [<small>]A[</small>]");
+
+}
+
+
+
+updateTime();
+setInterval(updateTime, 1000);
